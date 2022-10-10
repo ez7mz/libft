8450f6d882043589a1f68adea1d11e11cf89324e
Hamza Mesrar <hmesrar@e2r4p14.1337.ma>
2022-10-10 20:41:55 +0100

    part1

--- main.c
+++ main.c
@@ -2,6 +2,12 @@
 
 int	main(void)
 {
-	printf("%s\n", ft_strnstr(((void *)0), "fake", 3));
-	printf("%s\n", strnstr(((void *)0), "fake", 3));
+	// printf("%s\n", ft_strnstr(((void *)0), "fake", 3));
+	// printf("%s\n", strnstr(((void *)0), "fake", 3));
+
+	char hello[100] = "amongus";
+
+	printf("string before memecpy : %s\n",hello);
+	memcpy((hello + 5),hello,strlen(hello) + 1);
+	printf("string after memecpy : %s",hello);
 }
