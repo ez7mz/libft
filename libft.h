03ffc21133128b6a5ba476084b44be881ac73399
Hamza Mesrar <hmesrar@e1r3p11.1337.ma>
2022-10-11 23:30:56 +0100

    next

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: hmesrar <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/09 17:50:01 by hmesrar           #+#    #+#             */
-/*   Updated: 2022/10/11 14:55:24 by hmesrar          ###   ########.fr       */
+/*   Updated: 2022/10/11 23:22:53 by hmesrar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,9 @@ void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
-size_t	ft_strlcpy(char *restrict dst, const char *restrict src,
+size_t	ft_strlcpy(char *dst, const char *src,
 			size_t dstsize);
-size_t	ft_strlcat(char *restrict dst, const char *restrict src,
+size_t	ft_strlcat(char *dst, const char *src,
 			size_t dstsize);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
