569e2be17bf2842b3efb9d4fe7b245889db0e2dd
Hamza Mesrar <hmesrar@e1r5p15.1337.ma>
2022-10-13 20:28:55 +0100

    Final Mend

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,11 +6,26 @@
 /*   By: hmesrar <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 20:16:08 by hmesrar           #+#    #+#             */
-/*   Updated: 2022/10/13 20:16:35 by hmesrar          ###   ########.fr       */
+/*   Updated: 2022/10/13 20:27:01 by hmesrar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 void	ft_putnbr_fd(int n, int fd)
 {
-	return ;
+	if (n == -2147483648)
+		ft_putstr_fd("-2147483648", fd);
+	else if (n >= 0 && n <= 9)
+		ft_putchar_fd(n + 48, fd);
+	else if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		ft_putnbr_fd(-n, fd);
+	}
+	else
+	{
+		ft_putnbr_fd((n / 10), fd);
+		ft_putnbr_fd((n % 10), fd);
+	}
 }
